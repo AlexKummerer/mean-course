dc0a8ff14019cbf46fccfb34d5a57158418307cd
Kummey <alexander@thomas-kummerer.de>
2023-12-16 23:27:31 +0100

    #102 Implementing SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const express = require("express");
+const bcrypt = require("bcrypt");
+const  jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/user");
 
@@ -30,6 +30,52 @@ console.log(user);
         });
       });
   });
+});
+
+
+
+const jwt = require("jsonwebtoken");
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .exec()
+    .then((user) => {
+      console.log(user);
+      if (!user) {
+        return res.status(401).json({
+          message: "Authentication failed. User not found.",
+        });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authentication failed. Invalid password.",
+          });
+        }
+        if (result) {
+          console.log(result);
+          const token = jwt.sign(
+            { email: user.email, userId: user._id },
+            "secret_this_should_be_longer",
+            { expiresIn: "1h" }
+          );
+          return res.status(200).json({
+            message: "Authentication successful!",
+            token: token,
+          });
+        }
+        return res.status(401).json({
+          message: "Authentication failed. Invalid password.",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
