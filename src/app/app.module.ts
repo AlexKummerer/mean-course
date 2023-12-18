0001b94d2dd4cd2615e9f0104752becd09f5e44e
Kummey <alexander@thomas-kummerer.de>
2023-12-18 20:49:38 +0100

    #136 Creating the Auth Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
-import {  FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { HeaderComponent } from './header/header/header.component';
-
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/post.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     AngularMaterialModule,
-    PostsModule
+    PostsModule,
+    AuthModule,
   ],
   providers: [
     {
